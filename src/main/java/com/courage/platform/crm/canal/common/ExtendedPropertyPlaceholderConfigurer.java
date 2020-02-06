d9d97bd2d05d6f128786f3a98a57e88a8d463db1
zhangyong <zhangyong7120180@163.com>
2020-02-06 15:44:50 +0800

    place holder

---
+++ src/main/java/com/courage/platform/crm/canal/common/ExtendedPropertyPlaceholderConfigurer.java
@@ -0,0 +1,23 @@
+package com.courage.platform.crm.canal.common;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+import java.util.Properties;
+
+public class ExtendedPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
+
+    private Properties props;
+
+    @Override
+    protected void processProperties(ConfigurableListableBeanFactory beanFactory, Properties props) throws BeansException {
+        super.processProperties(beanFactory, props);
+        this.props = props;
+    }
+
+    public Object getProperty(String key) {
+        return props.get(key);
+    }
+    
+}
